7f7b8ef8593aa55929ab5f1009cf63311310e750
norbertax28 <norbertax28@gmail.com>
2023-12-06 15:08:56 -0500

    bug fixing in progress

--- __init__.py
+++ __init__.py
@@ -2,15 +2,14 @@ import psychrolib
 import math
 import pandas as pd
 from helper_functions import U_config_or_not
+from datetime import datetime
 psychrolib.SetUnitSystem(psychrolib.IP)
 #test Psychro
 TDewPoint = psychrolib.GetTDewPointFromRelHum(25.0, 0.80)
 print(TDewPoint)
 #read csv
 df = pd.read_csv('Book2.csv',header=None)
-#generate output excel
-excel_output = 'output_file.xlsx'
-df.to_excel(excel_output, index=False)
+
 #Assign columns
 columns_to_process_at = df.columns[2:11]  # Assuming 'C' is the third column (index 2)
 columns_to_process_lt = df.columns[12:17]  # Assuming 'M' is the 13th column (index 12)
@@ -40,10 +39,21 @@ for i, column in enumerate(columns_to_process_w, start=1):
 
 KWh = df[df.columns[36]]
 Watt = df[df.columns[37]]
-print(df)
-#Time_From_Start = df[df.columns[39]]
-#Total_Power_W = df[df.columns[40]]
+
+
+valid_times = pd.to_datetime(df.iloc[1:, 1], format='%H:%M:%S', errors='coerce')
+valid_times = valid_times.dt.time  # Extract time part only
+
+start_time = valid_times.dropna().iloc[0]
+v = valid_times.dt.time
+print(v)
+#df['time_difference'] = (pd.to_datetime(valid_times) - pd.to_datetime(start_time)).dt.total_seconds() / 3600
 
 #u_config = U_config_or_not(LT['2'],LT['4'])
 
+#Generate the output file
+#generate output excel
+excel_output = 'output_file.xlsx'
+df.to_excel(excel_output, index=False)
+#print(df[38])
     
